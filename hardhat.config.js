c603d3addb5ef03cde1539fba73a2021ec60197c
Dev_Pelz <pelzfx1@gmail.com>
2023-09-29 18:24:08 +0100

    modified: hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+const config = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.BASE_RPC,
+      accounts: [process.env.PRIVATEKEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
+
+export default config;
